c41b8a106a05f3e8f871a78b8d66798d31d2194c
abhishek729 <abhishekk@geakminds.com>
2020-09-21 13:56:48 +0530

    Change the layout and working functionality

---
+++ build/precache-manifest.55f7f0c736133d8bc592ebec6a76540e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9bf1130a93dd60a9fd6c8f6e9d6cbce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f42ae80a0b95a71f18",
+    "url": "/static/css/main.d0b5cc63.chunk.css"
+  },
+  {
+    "revision": "c564700534f53ea70868",
+    "url": "/static/js/2.1b04d2b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b04d2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f42ae80a0b95a71f18",
+    "url": "/static/js/main.f166b758.chunk.js"
+  },
+  {
+    "revision": "16f7e0cc22b35a0d07a1",
+    "url": "/static/js/runtime-main.a425c679.js"
+  }
+]);
